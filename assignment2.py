34979393360e5952ee21ac8c2ab3c7bb04a17593
Soham Manocha <77888711+manochasoham@users.noreply.github.com>
2021-02-09 17:21:27 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Altitude=input(" Pilot please tell us your  plane altitude ")
+if Altitude=="1000":
+    print("you are safe to land ")
+elif Altitude <="5000":
+    print("please bring down the plane to 1000ft")
+    
+else:
+    print("go around and try later ")
+
+
+# In[ ]:
+
+
+
+
